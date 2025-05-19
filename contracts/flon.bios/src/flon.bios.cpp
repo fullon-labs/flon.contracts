acc658d76dadb8bbcaa5e71f44e24b440b52f7d4
sirius4galaxy <uniswap_v2@163.com>
2025-05-19 10:49:13 +0800

    fix setalimits error in flon.bios contract

--- contracts/flon.bios/src/flon.bios.cpp
+++ contracts/flon.bios/src/flon.bios.cpp
@@ -95,7 +95,7 @@ void bios::setpriv( name account, uint8_t is_priv ) {
 
 void bios::setalimits( name account, uint64_t gas, bool is_unlimited ) {
    require_auth( get_self() );
-   set_resource_limits( account, is_unlimited, gas );
+   set_resource_limits( account, gas, is_unlimited );
 }
 
 void bios::setprods( const std::vector<eosio::producer_authority>& schedule ) {
